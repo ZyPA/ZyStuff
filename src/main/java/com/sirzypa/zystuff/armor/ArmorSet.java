26f0bb750e74c605dc7317eac020533627213efc
Sir. ZyPA <rytterkasper972@gmail.com>
2022-09-27 03:46:34 +0200

    -

--- src/main/java/com/sirzypa/zystuff/armor/ArmorSet.java
+++ src/main/java/com/sirzypa/zystuff/armor/ArmorSet.java
@@ -6,29 +6,21 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ArmorSet {
-    RegistryObject<ArmorItem> HEAD;
-    RegistryObject<ArmorItem> CHEST;
-    RegistryObject<ArmorItem> LEGS;
-    RegistryObject<ArmorItem> FEET;
-
+public class ArmorSet extends ModItems {
+    RegistryObject<ArmorItem> HEAD, CHEST, LEGS, FEET;
     public ArmorSet(String name, ArmorMaterial pMaterial, ArmorItem.Properties pProperties) {
-        this.HEAD = ModItems.ITEMS.register(name + "_helmet", () -> new ArmorItem(pMaterial, EquipmentSlot.HEAD, pProperties));
-        this.CHEST = ModItems.ITEMS.register(name + "_chestplate", () -> new ArmorItem(pMaterial, EquipmentSlot.CHEST, pProperties));
-        this.LEGS = ModItems.ITEMS.register(name + "_leggings", () -> new ArmorItem(pMaterial, EquipmentSlot.LEGS, pProperties));
-        this.FEET = ModItems.ITEMS.register(name + "_boots", () -> new ArmorItem(pMaterial, EquipmentSlot.FEET, pProperties));
+        this.HEAD = ITEMS.register(name + "_helmet", () -> new ArmorItem(pMaterial, EquipmentSlot.HEAD, pProperties));
+        this.CHEST = ITEMS.register(name + "_chestplate", () -> new ArmorItem(pMaterial, EquipmentSlot.CHEST, pProperties));
+        this.LEGS = ITEMS.register(name + "_leggings", () -> new ArmorItem(pMaterial, EquipmentSlot.LEGS, pProperties));
+        this.FEET = ITEMS.register(name + "_boots", () -> new ArmorItem(pMaterial, EquipmentSlot.FEET, pProperties));
     }
-
     public RegistryObject<ArmorItem> getHead() { return HEAD; }
-
     public RegistryObject<ArmorItem> getChest() {
         return CHEST;
     }
-
     public RegistryObject<ArmorItem> getLegs() {
         return LEGS;
     }
-
     public RegistryObject<ArmorItem> getFeet() {
         return FEET;
     }
